2fa7cee5d5ef05d447dea35f48c35fc5fcd63fb7
MrfksIv <cmorphakis@gmail.com>
2017-10-14 19:34:29 +0300

    continued with chapter6

--- Accelerated_CPP_Chapter_06/Accelerated_CPP_Chapter_06/AcceleratedCPPChapter06.cpp
+++ Accelerated_CPP_Chapter_06/Accelerated_CPP_Chapter_06/AcceleratedCPPChapter06.cpp
@@ -67,13 +67,103 @@ vector<string> split(const string& str) {
 		// The next find_if() does the opposite: Stops at the first instance of a white-space 
 		// character.
 		iter j = std::find_if(i, str.end(), space);
-
+		
 		if (i != str.end()) {
+			// stdd:string::substr only works on indices, NOT iterators. Fortunately, string
+			// can be initialised in the way below by taking two iterators and returning  the 
+			//characters in the range [i, j).
 			ret.push_back(string(i, j));
 		}
 		i = j;
 	}
 }
+
+bool notUrlChar(char c) {
+	// local variables that are declared as static are preserved across invocations of the function.
+	// This means the variable will be constructed and initialised only on the first invocation of 
+	// the function. Subsequent invocations will use the object that first constructed.
+	static const string url_ch = "~;/?:@=&$-_.+!*`(),";
+
+	// isalnum is defined in the <cctype> header and tests whether its argument is alphanumeric.
+	// find() looks for a specific value given as its 3rd argument. If the argument is present, it
+	// returns an iterator denoting the first occurence. If the value is not present, it returns its
+	// second argument.
+	//
+	// Because the return value is negated, the function will return false if c is alphanumeric or, one
+	// of url_ch. Otherwise, it returns true.
+	return !(std::isalnum(c) || std::find(url_ch.begin(), url_ch.end(), c) != url_ch.end());
+}
+
+// url_end will thus return an iterator at the first occurence of a non-url character, if one exists. 
+// Otherwise, it will return its second argument (iter e).
+string::const_iterator url_end(string::const_iterator b, string::const_iterator e) {
+	return std::find_if(b, e, notUrlChar);
+}
+
+string::const_iterator url_begin(string::const_iterator b, string::const_iterator e) {
+	static const string sep = "://";
+	typedef string::const_iterator iter;
+
+	iter i = b;
+
+	// search takes two pairs of iterators: The first pair denotes the sequence in which we are looking,
+	// and the second one denotes the string we are looking for. 
+	// As with the other functions, if search fails, the second iterator is returned. This means that
+	// after the call to search, i either denotes (one past) the end of the input string (e), or it a denotes
+	// a ':' followed by '//'.
+	while ((i = std::search(i, e, sep.begin(), sep.end())) != e) {
+			
+		// After finding the separator, we should isolate the protol if any. Before we do that, we check that
+		// the separator is neither at the beginning, nor the end of the string.
+		if (i != e && i + sep.size() != e) {
+
+			// beg marks the beginning of the protocol-name
+			iter beg = i;
+			// The following while loop moves beg backwards through the input until it has either reached the
+			//beginning of the string or a non-alphabetic character. 
+			//Notice that if a container supports indexing, so do its iterators: beg[-1] is equivalent to *(beg - 1)
+			while (beg != b && std::isalpha(beg[-1])) {
+				--beg;
+			}
+
+			if (beg != i && !notUrlChar(i[sep.size()])) {
+				return beg;
+			}
+		}
+		i += sep.size();
+	}
+	return e;
+}
+
+vector<string> findURLs(const string & s) {
+	vector<string> ret;
+	typedef string::const_iterator iter;
+	iter b = s.begin(), e = s.end();
+
+	// look through the entire input string
+	while (b != e) {
+		// look for one or more letters followed by ://
+		b = url_begin(b, e);
+		
+		// if we found it 
+		if (b != e) {
+			// get the rest of the URL
+			iter after = url_end(b, e);
+			
+			// add the newly found URL to our result vector
+			ret.push_back(string(b, after));
+			
+			// advance b and check for more urls
+			b = after;
+		}
+	}
+	return ret;
+}
+
+
+
+
+
 
 int main()
 {
